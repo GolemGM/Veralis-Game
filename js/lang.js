5374f39c0bc70fcfd8bed9feb35740f01baa51fd
GolemGM <golem.gm@seznam.cz>
2025-08-02 11:33:23 +0200

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -3,6 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch(`lang/${lang}.json`)
     .then(res => res.json())
     .then(data => {
+      // Nastaví title záložky (prohlížeč)
+      if (data?.meta?.title) {
+        document.title = data.meta.title;
+      }
+
+      // Projde všechny prvky s data-i18n a přeloží je
       document.querySelectorAll("[data-i18n]").forEach(el => {
         const key = el.getAttribute("data-i18n");
         const value = key.split('.').reduce((o, i) => o?.[i], data);
